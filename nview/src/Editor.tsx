2c424698c948e67be3aa4ea4933e8831f0c7a0c0
Albert Ford <albert@albertford.com>
2025-12-13 22:14:08 -0800

    Add door-switch lines

--- nview/src/Editor.tsx
+++ nview/src/Editor.tsx
@@ -32,6 +32,13 @@ const ENTITY_EXIT = 3;
 const BONES_STANDING = new Float32Array([-0.039, -0.0249, 0.1127, -0.1738, 0.1115, -0.1512, -0.0846, 0.0749, 0.1072, -0.0423, 0.0263, -0.1452, -0.0358, -0.075, -0.377, 0.4686, 0.4643, -0.0225, -0.0453, -0.5054, -0.4724, 0.1962, 0.2293, -0.1812, -0.2266, -0.2224]);
 const BONES_FALLING = new Float32Array([0.018, 0.0, 0.4156, 0.0988, 0.3581, -0.3242, -0.0708, 0.0845, 0.2924, 0.3212, 0.1853, -0.1927, -0.0236, -0.06, -0.3602, 0.3086, 0.1278, -0.3238, -0.2018, -0.4976, -0.4488, 0.0656, -0.024, -0.2729, -0.3268, -0.2042]);
 
+type Line = {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+};
+
 export function EditorApp() {
     const editor = Editor.new();
 
@@ -50,6 +57,8 @@ export function EditorApp() {
     const [previewNinjas, setPreviewNinjas] = createSignal<NinjaData[]>([]);
     const [previewExitDoors, setPreviewExitDoors] = createSignal<ExitDoorData[]>([]);
     const [previewExitSwitches, setPreviewExitSwitches] = createSignal<ExitSwitchData[]>([]);
+
+    const [doorSwitchLines, setDoorSwitchLines] = createSignal<Line[]>([]);
 
     document.addEventListener('keydown', event => {
         let change = false;
@@ -128,6 +137,8 @@ export function EditorApp() {
             y: editor.crosshair_y(),
         });
 
+        const lines: Line[] = [];
+
         const ninjas: NinjaData[] = [];
         const exitDoors: ExitDoorData[] = [];
         const exitSwitches: ExitSwitchData[] = [];
@@ -153,6 +164,12 @@ export function EditorApp() {
                         x: entity.switch_x,
                         y: entity.switch_y,
                         animProgress: 0,
+                    });
+                    lines.push({
+                        x1: entity.x,
+                        y1: entity.y,
+                        x2: entity.switch_x,
+                        y2: entity.switch_y,
                     });
                 }
             }
@@ -188,6 +205,12 @@ export function EditorApp() {
                         y: entity.switch_y,
                         animProgress: 0,
                     });
+                    lines.push({
+                        x1: entity.x,
+                        y1: entity.y,
+                        x2: entity.switch_x,
+                        y2: entity.switch_y,
+                    });
                 }
             }
             // Do I need this?
@@ -197,6 +220,8 @@ export function EditorApp() {
         setPreviewNinjas(previewNinjas);
         setPreviewExitDoors(previewExitDoors);
         setPreviewExitSwitches(previewExitSwitches);
+
+        setDoorSwitchLines(lines);
     }
 
     const regularGridXs = [];
@@ -270,6 +295,9 @@ export function EditorApp() {
             <ExitSwitches exitSwitches={[previewExitSwitches, () => {}]} />
             <For each={previewNinjas()}>
                 {ninja => <Ninja class="ninja" ninja={() => ninja} bones={() => BONES_STANDING} />}
+            </For>
+            <For each={doorSwitchLines()}>
+                {line => <line class="door-switch-line" x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2} />}
             </For>
             <Show when={mode() === MODE_PAINT_TILES}>
                 <use href="#tilemode-crosshair" x={tilemodeCrosshairPos().col * 24 + 12} y={tilemodeCrosshairPos().row * 24 + 12} />
