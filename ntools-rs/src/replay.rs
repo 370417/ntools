4964337e08bf5bc4232625bf58eb851f9c50c8ec
Albert Ford <albert@albertford.com>
2026-05-03 21:52:26 -0700

    Remove old attract code

--- ntools-rs/src/replay.rs
+++ ntools-rs/src/replay.rs
@@ -4,7 +4,7 @@ use futures_channel::oneshot::Sender;
 use glam::{DVec2, FloatExt};
 use wasm_bindgen::prelude::*;
 
-use crate::{anim_data::flatten_bones, editor::Editor, entity::mine::Mine, grid::{COLS, Grid, ROWS}, map_file::MapFile, ninja::{Ninja, NinjaState, PastNinja}, orientation::OrientationExt, segment::{Segment, extract_path}, simulation::{Input, KeyFrame, Simulation}, tile::TILE_SIZE};
+use crate::{anim_data::flatten_bones, attract::to_attract_bytes, editor::Editor, entity::mine::Mine, grid::{COLS, Grid, ROWS}, map_file::MapFile, ninja::{Ninja, NinjaState, PastNinja}, orientation::OrientationExt, segment::{Segment, extract_path}, simulation::{Input, KeyFrame, Simulation}, tile::TILE_SIZE};
 
 #[wasm_bindgen]
 pub struct Replay {
@@ -531,7 +531,7 @@ impl Replay {
     }
 
     pub fn export_attract(&self, editor: &Editor) -> Box<[u8]> {
-        MapFile::to_attract(&editor.export_map(), &self.inputs).into()
+        to_attract_bytes(&editor.export_map(), &self.inputs).into()
     }
 }
 
@@ -558,8 +558,6 @@ impl Replay {
 
 #[cfg(test)]
 mod tests {
-    use crate::attract::Attract;
-
     use super::*;
 
     #[test]
@@ -658,30 +656,8 @@ mod tests {
 
     impl Replay {
         fn from_attract(attract_bytes: &[u8]) -> Result<Replay, String> {
-            let Attract { level_name, author_name, tile_segments, ninjas, entities, inputs, .. } = Attract::from_bytes(attract_bytes)?;
-
-            let mut segments = tile_segments;
-            entities.doors.populate_grid(&mut segments);
-
-            let current_sim = Simulation::new(ninjas, entities, false)?;
-
-            let mut keyframes = BTreeMap::new();
-            keyframes.insert(0, KeyFrame::from_sim(&current_sim, &current_sim.entities.mines));
-
-            Ok(Replay {
-                _level_name: level_name,
-                _author_name: Some(author_name),
-                segments,
-                inputs,
-                past_ninjas: Vec::new(),
-                initial_mines: current_sim.entities.mines.clone(),
-                preview_sim: current_sim.clone(),
-                current_sim,
-                keyframes,
-                sender: None,
-                anim_data: Box::new([]),
-                is_from_attract: true,
-            })
+            let mut editor = Editor::new();
+            editor.load_attract(attract_bytes, false, false)
         }
     }
 }
